40dcb9186b0b39b74d255f37c8288988c8f06f42
Steven L Lerner <stevenllerner@gmail.com>
2017-10-13 22:31:54 -0500

    initial setup

---
+++ ClozeCard.js
@@ -0,0 +1,14 @@
+
+var ClozeCard = function (text, cloze){
+	this.cloze: cloze,
+	this.partial: partial,
+	this.fullText: text,
+	
+pseudo code add the below using a prototype
+
+	this.error: function(){
+		pseudo code: if cloze is not contained in text, then report an error
+	}
+};
+
+module.exports = ClozeCard;
